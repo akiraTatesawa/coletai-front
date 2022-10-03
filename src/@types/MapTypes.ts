8bf79b74cecca10cc209be5a6cd2897b438ce4b9
Arthur Akira <arthurak10.at@gmail.com>
2022-10-03 14:27:12 -0300

    feat: add fetchAddress function

--- src/@types/MapTypes.ts
+++ src/@types/MapTypes.ts
@@ -1,1 +1,17 @@
 export type Coords = [number, number];
+
+type NominatimAddress = {
+  road: string;
+  village: string;
+  state_district: string;
+  state: string;
+  postcode: string;
+  country: string;
+  country_code: string;
+};
+
+export interface NominatimResponse {
+  display_name: string;
+  name: string;
+  address: NominatimAddress;
+}
