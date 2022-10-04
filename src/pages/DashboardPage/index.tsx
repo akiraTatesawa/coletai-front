a6b1eb83dae4ce46046d501339a3c89d927e4085
Arthur Akira <arthurak10.at@gmail.com>
2022-10-04 11:37:59 -0300

    feat: create dashboard page component

---
+++ src/pages/DashboardPage/index.tsx
@@ -0,0 +1,14 @@
+import { Title } from "../../components/Title";
+import { Main } from "../FrontPage/styles";
+import { CollectionsContainer, CollectionsTitle } from "./styles";
+
+export function DashboardPage() {
+  return (
+    <Main>
+      <Title />
+      <CollectionsContainer>
+        <CollectionsTitle>Suas coletas</CollectionsTitle>
+      </CollectionsContainer>
+    </Main>
+  );
+}
