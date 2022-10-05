0360a4d233a649c8cc73b6eb8337d1212ce5f446
Arthur Akira <arthurak10.at@gmail.com>
2022-10-05 16:37:01 -0300

    feat: add colors to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          100: "var(--surface-brand)",
+          200: "#AFF9BB",
+          400: "var(--brand)",
+          500: "var(--brand-hover)",
+          700: "#1A9268",
+        },
+        error: {
+          100: "#FDE1D3",
+          400: "#E9635A",
+          600: "#BC1D2E",
+        },
+        "brand-text": {
+          primary: "#22394A",
+          secondary: "#6C7C80",
+        },
+      },
+    },
   },
   plugins: [],
 };
