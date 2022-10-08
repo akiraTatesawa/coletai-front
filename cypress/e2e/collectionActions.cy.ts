2e512d8d08cd119b1fb9e4d618c3e6989f9270ae
Arthur Akira <arthurak10.at@gmail.com>
2022-10-08 17:13:03 -0300

    test: add finish collection cypress test

--- cypress/e2e/collectionActions.cy.ts
+++ cypress/e2e/collectionActions.cy.ts
@@ -3,7 +3,7 @@ describe("Collection Actions", () => {
     cy.resetDatabase();
   });
 
-  it("Should be able to cancel a recommendation", () => {
+  it("Should be able to cancel a collection", () => {
     cy.createCollection().then(({ email, password }) => {
       cy.loginAccount({ email, password }, "cooperatives").then(() => {
         cy.visit("http://localhost:5173/");
@@ -24,6 +24,47 @@ describe("Collection Actions", () => {
         cy.wait("@cancelCollection");
 
         cy.get(".Toastify").should("contain", "Coleta cancelada");
+      });
+    });
+  });
+
+  it("Should be able to click on cancel collection and then exit the modal", () => {
+    cy.createCollection().then(({ email, password }) => {
+      cy.loginAccount({ email, password }, "cooperatives").then(() => {
+        cy.visit("http://localhost:5173/");
+
+        cy.get("[data-cy='dashboard']").click();
+
+        cy.url().should("equal", "http://localhost:5173/dashboard");
+
+        cy.get("[data-cy='button-cancel-collection']").click();
+
+        cy.get("[data-cy='button-exit-cancel-modal']").click();
+
+        cy.get("[data-cy='button-cancel-collection']").should("be.visible");
+      });
+    });
+  });
+
+  it("Should be able to finish a collection", () => {
+    cy.createCollection().then(({ email, password }) => {
+      cy.loginAccount({ email, password }, "cooperatives").then(() => {
+        cy.visit("http://localhost:5173/");
+
+        cy.get("[data-cy='dashboard']").click();
+
+        cy.url().should("equal", "http://localhost:5173/dashboard");
+
+        cy.intercept({
+          method: "PATCH",
+          url: /finish$/,
+        }).as("finishCollection");
+
+        cy.get("[data-cy='button-finish-collection']").click();
+
+        cy.wait("@finishCollection");
+
+        cy.get(".Toastify").should("contain", "Coleta finalizada");
       });
     });
   });
